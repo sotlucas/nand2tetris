4d1db85f8959a996b647a6e74c9308162efbea49
Lucas Sotelo <lucasnsotelog@gmail.com>
2019-08-30 17:29:04 -0300

    Finished Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,5 +10,67 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+    
+    @8191
+    D=A
+    @max
+    M=D // max = 8191
 
-// Put your code here.
+
+(WAIT)
+    @i
+    M=0 // i = 0
+    @SCREEN
+    D=A
+    @addr
+    M=D // addr = SCREEN
+    @KBD
+    D=M
+    @BLACKEN
+    D;JNE // if KBD == 0 then goto BLACKEN
+    @WHITEN
+    0;JMP // else goto WHITEN
+
+(BLACKEN)
+    @i
+    D=M
+    @max
+    D=D-M
+    @WAIT
+    D;JGT
+    
+    @addr
+    A=M
+    M=-1 // RAM[SCREEN + i] = -1
+    @addr
+    M=M+1
+
+    @i
+    M=M+1 // i = i + 1
+     
+    @BLACKEN
+    0;JMP
+
+(WHITEN)
+    @i
+    D=M
+    @max
+    D=D-M
+    @WAIT
+    D;JGT
+    
+    @addr
+    A=M
+    M=0 // RAM[SCREEN + i] = 0
+    @addr
+    M=M+1
+
+    @i
+    M=M+1 // i = i + 1
+     
+    @WHITEN
+    0;JMP
+
+(END)
+    @END
+    0;JMP
